079eb9798cb1c2a3d7b47767d93a688ff752217e
GabrielZapataok2 <gabriel.zapata@reserv.com>
2024-11-13 19:32:01 -0300

    parallax slide

--- swiper.js
+++ swiper.js
@@ -1,22 +1,20 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'vertical',
+    parallax: true,
     loop: true,
     autoplay: {
         delay: 5000,
     },
-
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-
     // Navigation arrows
     navigation: {
         // nextEl: '.swiper-button-next',
         // prevEl: '.swiper-button-prev',
     },
-
     // And if we need scrollbar
     scrollbar: {
         // el: '.swiper-scrollbar',
